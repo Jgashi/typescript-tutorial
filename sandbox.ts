f70eebf826b13bdc0c44f0a6ac885fcb4e4d2d08
Jgashi <40315535+Jgashi@users.noreply.github.com>
2022-01-05 17:46:57 +0800

    finish

--- sandbox.ts
+++ sandbox.ts
@@ -5,15 +5,14 @@ let isBlackBelt = false;
 // character = 20;
 character = 'luigi';
 
-// age = 'yoshi';
+// age= 'yoshi'
 age = 40;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+//isBlackBelt = 'yes';
+isBlackBelt = true
 
 const circ = (diameter: number) => {
   return diameter * Math.PI;
-};
+}
 
-// console.log(area('hello'));
 console.log(circ(7.5));
